5368b70a9ba2bb5b1db29f1018e93f9cbea25835
Emre Uzun <uzun.emre1@outlook.com>
2022-08-04 11:12:55 +0300

    First test :: added, Detail Page :: implemented

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,19 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { render, screen, cleanup } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./rxutils/store";
 import App from "./App";
 
 afterEach(cleanup);
 
-test("renders learn react link", () => {
-  const { queryByTestId } = render(<App />);
+test("renders image in home page", () => {
+  const { queryByTestId } = render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
   expect(queryByTestId("wallpaper")).toBeInTheDocument();
 });
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router";
+import { useSelector } from "react-redux";
 
 const Detail = () => {
+  const { units } = useSelector((state) => state.units);
   const { id } = useParams();
-  console.log(id);
-  return <div></div>;
+  const [data, setData] = useState(units[id - 1]);
+
+  return (
+    <div className="detail">
+      <ul>
+        {Object.entries(data).map(([key, value] = entry, index) => {
+          if (key === "cost" && value) {
+            return (
+              <>
+                {value.Wood && (
+                  <li>
+                    <div className="key">Wood</div>
+                    <div className="value">{value.Wood}</div>
+                  </li>
+                )}
+                {value.Food && (
+                  <li>
+                    <div className="key">Food</div>
+                    <div className="value">{value.Food}</div>
+                  </li>
+                )}
+                {value.Gold && (
+                  <li>
+                    <div className="key">Gold</div>
+                    <div className="value">{value.Gold}</div>
+                  </li>
+                )}
+              </>
+            );
+          }
+          return (
+            <li key={index}>
+              <div className="key">{key}</div>
+              <div className="value">{value}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Detail;
